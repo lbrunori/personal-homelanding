9c3d85b5fb2d2752543fa219827912b128130f95
Lucas Brunori <lucas.brunori@gmail.com>
2021-07-04 11:59:52 -0300

    First version of navbar and introduction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import Navbar from './Navbar/Navbar';
+import Introduction from './Introduction/Introduction';
+import WorkExperience from './WorkExperience/WorkExperience';
+
 import './App.css';
+
+
+      //TODO: navbar
+      //TODO: main section
+      //TODO: work experience
+      //TODO: technologies
+      //TODO: footer contact date
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
       </header>
+      <main>
+        <Introduction />
+        <WorkExperience />
+      </main>
     </div>
   );
 }
